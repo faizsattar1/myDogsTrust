8b039b8c9c8c15548258a0d92e24593270532f5d
faizsattar1 <faizsattar3@gmail.com>
2021-10-28 16:38:01 +0500

    added changes

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 "use strict"
 
 let dogs=[]
+function setupDogs (){
 
-dogs.push({name:"Fluffy",Breed:"Poodle",picture:"/Images/Fluffy",age:1})
-dogs.push({name:"Otto",Breed:"Chihuahua",picture:"/Images/Otto",age:2})
-dogs.push({name:"Alfie",Breed:"French Bulldog",picture:"/Images/Alfie", age:3})
-dogs.push({name:"Perkins",Breed:"American Bulldog",picture:"/Images/Perkins", age:4})
-dogs.push({name:"Tyson",Breed:"Rottweiler",picture:"/Images/Tyson", age:5})
-dogs.push({name:"Rocky",Breed:"German Shepherd",picture:"/Images/Rocky", age:6})
-
+dogs.push(new Dog("Fluffy","Poodle","/Images/Fluffy","Age:1"))
+dogs.push(new Dog("Otto","Chihuahua","/Images/Otto","Age:2"))
+dogs.push(new Dog("Alfie","French Bulldog","/Images/Alfie", "Age:3"))
+dogs.push(new Dog("Perkins","American Bulldog","/Images/Perkins", "Age:4"))
+dogs.push(new Dog("Tyson","Rottweiler","/Images/Tyson", "Age:5"))
+dogs.push(new Dog("Rocky","German Shepherd","/Images/Rocky", "Age:6"))
+}
 // add an 'Adopt Now' button on each dog
-
+function showDogs () {
 for(let i=0;i<dogs.length;i++){
 
     let card = document.createElement("div")
@@ -39,10 +40,21 @@ for(let i=0;i<dogs.length;i++){
     adopt.innerHTML= "Adopt now"
     card.appendChild(adopt)
     adopt.classList.add("adopt")
+}
+}
 
+class Dog{
 
-
+constructor(name, Breed, picture, age){
+    
+    this.name = name
+    this.Breed = Breed
+    this.picture = picture + ".jpg"
+    this.age = age
+}
 
 
 }
 
+setupDogs ()
+showDogs ()
